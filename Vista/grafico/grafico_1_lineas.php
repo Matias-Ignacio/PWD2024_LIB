dc10a618f0fff8c18db18eec3727337e3a923fef
ignacio bustos <nachobustos@live.com.ar>
2024-10-10 20:47:54 -0300

    Agrege algunos comentarios

--- Vista/grafico/grafico_1_lineas.php
+++ Vista/grafico/grafico_1_lineas.php
@@ -9,31 +9,31 @@ $datay3 = array(5,17,32,24,4,2,36,2,9,24,21,23);
 
 // Setup the graph
 $graph = new Graph(900,350);
-$graph->SetScale("textlin");
+$graph->SetScale("textlin"); //Indica  que el eje X es tipo texto y el eje Y es lineal. 
 
 $theme_class = new UniversalTheme;
-
+                                    // Se aplicaun estilo al grafico. Ajusta el estilo visual general, como colores de fondo, fuentes y detalles graficos
 $graph->SetTheme($theme_class);
-$graph->img->SetAntiAliasing(false);
+$graph->img->SetAntiAliasing(false); // desactiva  temporalmente el suavizado de bordes para optimizar el rendimiento grafico al principio
 $graph->title->Set('Evolucion de pedidos');
 $graph->SetBox(false);
 
-$graph->img->SetAntiAliasing();
+$graph->img->SetAntiAliasing(); //Se habilita el suavizado para mejorar la calidad visual del gráfico.
 
-$graph->yaxis->HideZeroLabel();
-$graph->yaxis->HideLine(false);
-$graph->yaxis->HideTicks(false,false);
+$graph->yaxis->HideZeroLabel(); // Oculta  la etiqueta  del valor cero en el eje Y
+$graph->yaxis->HideLine(false); // Muestra la línea del eje Y
+$graph->yaxis->HideTicks(false,false); // Muestra  las marcas de division principales y secundarias en el eje Y
 
-$graph->xgrid->Show();
-$graph->xgrid->SetLineStyle("solid");
-$graph->xaxis->SetTickLabels(array('Ene','Feb','Mar','Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'));
-$graph->xgrid->SetColor('#E3E3E3');
+$graph->xgrid->Show(); // Muestra la cuadrícula del eje X
+$graph->xgrid->SetLineStyle("solid"); // Establece que las lineas de la cuadricula sean solidas
+$graph->xaxis->SetTickLabels(array('Ene','Feb','Mar','Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic')); // Se establece las etiquetas para las marcas del eje X  como  los meses del año.
+$graph->xgrid->SetColor('#E3E3E3');// Define el color de la cuadricula como un gris claro
 
 // Create the first line
-$p1 = new LinePlot($datay1);
-$graph->Add($p1);
+$p1 = new LinePlot($datay1); // Se crea un gráfico de línea para los datos $datay1
+$graph->Add($p1); // El grafico de linea se añade al grafico principal
 $p1->SetColor("#6495ED");
-$p1->SetLegend('Tienda 1');
+$p1->SetLegend('Tienda 1'); // se añade una etiqueta  "Tienda 1" para esta linea
 
 // Create the second line
 $p2 = new LinePlot($datay2);
@@ -47,9 +47,9 @@ $graph->Add($p3);
 $p3->SetColor("#FF1493");
 $p3->SetLegend('Tienda 3');
 
-$graph->legend->SetFrameWeight(1);
+$graph->legend->SetFrameWeight(1); // Establece un marco alrededor de la leyenda con una grosor de 1 pixal.
 
-$graph->legend->SetPos(0.5,0.98,'center','bottom');
+$graph->legend->SetPos(0.5,0.98,'center','bottom'); // coloca la leyenda en el centro en la parte inferior del grafico
 
 // Output line
 $graph->Stroke();
